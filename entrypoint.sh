dd1b47e349efc8e80d5cae23a985282723b4baf0
Gaurab Manandhar <60678135+gmanandhar@users.noreply.github.com>
2021-01-25 11:01:57 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,41 +2,23 @@
 
 set -e
 
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='172.17.0.3'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
-
-DB_ARGS=()
-function check_config() {
-    param="$1"
-    value="$2"
-    if ! grep -q -E "^\s*\b${param}\b\s*=" "$OPENERP_SERVER" ; then
-        DB_ARGS+=("--${param}")
-        DB_ARGS+=("${value}")
-   fi;
-}
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
+# set odoo database host, port, user and password
+: ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
+: ${PGPORT:=$DB_PORT_5432_TCP_PORT}
+: ${PGUSER:=${DB_ENV_POSTGRES_USER:='postgres'}}
+: ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+export PGHOST PGPORT PGUSER PGPASSWORD
 
 case "$1" in
-    -- | openerp-server)
-        shift
-        if [[ "$1" == "scaffold" ]] ; then
-            exec openerp-server "$@"
-        else
-            exec openerp-server "$@" "${DB_ARGS[@]}"
-        fi
-        ;;
-    -*)
-        exec openerp-server "$@" "${DB_ARGS[@]}"
-        ;;
-    *)
-        exec "$@"
+	--)
+		shift
+		exec openerp-server "$@"
+		;;
+	-*)
+		exec openerp-server "$@"
+		;;
+	*)
+		exec "$@"
 esac
 
 exit 1
